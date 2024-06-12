99440516939b0338a9bbc28ea4c8bb5cae1a4e1a
mikepotpinka61 <152926418+mikepotpinka61@users.noreply.github.com>
2024-06-11 21:39:18 -0400

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -13,6 +13,8 @@ BASE_URL = "/accounts"
 ############################################################
 # Health Endpoint
 ############################################################
+
+
 @app.route("/health")
 def health():
     """Health Status"""
@@ -84,6 +86,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+    
 
 def test_get_account_not_found(self):
     """It should not Read an Account that is not found"""
